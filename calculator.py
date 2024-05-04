0334b33e10c2f3491c1e7e0fed63c20797c70aa5
Oxmalik <muhammadm4lik@gmail.com>
2024-05-04 18:03:23 -0400

    calculator based on operations in python

---
+++ calculator.py
@@ -0,0 +1,17 @@
+print('enter first number')
+n1 = int(input()) # Don't change this line
+print('enter second number')
+n2 = int(input()) # Don't change this line
+print('enter operation')
+op = input() # Don't change this line
+result = 0
+
+if op == '+':
+    result=n1+n2
+elif op == '-':
+    result=n1-n2
+elif op == '/':
+    result=n1/n2
+elif op =='*':
+    result=n1*n2
+print (result)
--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
 #Muhammad Malik
 #Functions
 #7/25/2022
+
+print("Hello")
