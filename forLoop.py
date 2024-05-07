0cdc06c86484edb4d85a9cf208420def92306bf7
Oxmalik <muhammadm4lik@gmail.com>
2024-05-06 23:29:19 -0400

    solution

--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,5 @@
 #Write a program that prints "Hello Coddy: " and the i value from 3 to 27 (including, 25 times in total), do it using a for loop.
+
+#for loop 
+for i in range(3, 28):
+    print(f'Hello Coddy: {i}')
