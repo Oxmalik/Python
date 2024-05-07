1a3e0d9a0526fe5b9a21cea661b8172ffa6e3597
Oxmalik <muhammadm4lik@gmail.com>
2024-05-07 00:25:03 -0400

    while loop

---
+++ whileLoop.py
@@ -0,0 +1,14 @@
+#Dev: Muhammad Malik
+#Program: Basic While Loop
+#5/7/2024
+
+#Prompt:
+# Write a program that gets one input, float number.
+#Use a while loop to divide the input by 2 as long as the number is bigger or equal to 3.5.
+#Print the first number that is smaller than 3.5.
+
+number = float(input())
+
+while number >= 3.5:
+    number/=2
+print(number)
