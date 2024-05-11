51a322845a9cf1506d494cd3c383c6c5c3ce5583
Oxmalik <muhammadm4lik@gmail.com>
2024-05-10 23:37:54 -0400

    arguments prompt

---
+++ functionArgs.py
@@ -0,0 +1,13 @@
+#Muhammad Malik
+#functionArgs
+#5/10/2024
+
+
+"""
+Write a program that gets two inputs, numbers. The input numbers are the arguments of the below function. 
+
+Create a function that gets two arguments, calculates the product of them and prints it, name the function however you like.
+
+Call the function with the input numbers.
+
+"""
