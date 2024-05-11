752c5c5cf6c4334bc552c773cbe7c9faa8265c01
Oxmalik <muhammadm4lik@gmail.com>
2024-05-10 23:41:02 -0400

    function with args

--- functionArgs.py
+++ functionArgs.py
@@ -11,3 +11,11 @@ Create a function that gets two arguments, calculates the product of them and pr
 Call the function with the input numbers.
 
 """
+
+def prod(n1, n2):
+    p = n1 * n2
+    print(p)
+
+num1 = int(input())
+num2 = int(input())
+prod(num1, num2)
