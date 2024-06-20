491fcd757bf141634d61845a9ccc4567f230bfbf
Oxmalik <muhammadm4lik@gmail.com>
2024-06-20 19:13:03 -0400

    added prompt

--- return2.py
+++ return2.py
@@ -1,3 +1,19 @@
+
+"""
+Each test case has three inputs.
+
+The first input indicates how many times to do iterations, and the last two inputs are numbers that we will do operations on.
+
+Create a function that receives two arguments and returns the bigger number of the two. if both are equal then return one of them.
+
+Iterate 'iterations' times and for each iteration do:
+
+1. Call the function with num1, num2, and save the result in a variable.
+2. Divide the bigger number of the two by 2, and then replace the original larger variable with the new result value.
+3. print the new value.  
+4. Continue doing it until the program iterated iterations times or one of the numbers is smaller than 2.
+"""
+
 iterations = int(input())
 num1 = int(input())
 num2 = int(input())
