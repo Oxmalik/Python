3aa889c7e21c083030e7044e1676cdd04c1a8713
Oxmalik <muhammadm4lik@gmail.com>
2024-10-29 22:35:59 -0400

    simple list

---
+++ list.py
@@ -0,0 +1,7 @@
+# Muhammad Malikj
+# creating a simple list
+# 10/29/2024
+
+"""Create a list called shopping_list that contains the following items: bread, eggs, milk, and butter."""
+
+shopping_list = ['bread', 'eggs', 'milk', 'butter']
