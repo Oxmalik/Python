a29aee0c6f9ca22ceb68c81289f23478727d1465
Oxmalik <muhammadm4lik@gmail.com>
2024-11-05 23:25:11 -0500

    hello world

---
+++ basicPythonChallenges-practice/helloWorld.py
@@ -0,0 +1,12 @@
+# Muhammad Malik
+# Hello World
+# difficulty Easy
+# 11/05/2024
+
+'''Print Hello World! to the screen,
+ in a function named hello!'''
+
+def hello():
+    print('Hello World!')
+
+hello()
