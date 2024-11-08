19a04d63f6b946b42ab694efd73660d02d8187d7
Oxmalik <muhammadm4lik@gmail.com>
2024-11-07 23:47:56 -0500

    greet

---
+++ basicPythonChallenges-practice/greet.py
@@ -0,0 +1,12 @@
+# Muhammad Malik
+# Max Number
+# difficulty Easy
+# 11/06/2024
+
+'''Write a function named greet
+that gets a name as input and prints Hello, name .'''
+
+def greet(name):
+    print('hello, ',name)
+
+greet('my guy')
