56fdfb349c4b9b5af50aab9282fbd50f26503327
Oxmalik <muhammadm4lik@gmail.com>
2024-11-14 00:53:44 -0500

    transpose setup

---
+++ basicPythonChallenges-practice/transpose.py
@@ -0,0 +1,11 @@
+# Muhammad Malik
+# transpose
+# difficulty Medium
+# 11/12/2024
+
+'''Create a function names transpose that receives 
+one argument: A list that contains lists. and returns a modified list.
+
+Your task is to swap the rows and columns of the list. 
+This is usually referred to as transpose in mathematics.'''
+
