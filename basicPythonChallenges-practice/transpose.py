53a6ebe80746705ffc4bb828eb70a5e1e807ea86
Oxmalik <muhammadm4lik@gmail.com>
2024-11-16 23:04:39 -0500

    transpose update

--- basicPythonChallenges-practice/transpose.py
+++ basicPythonChallenges-practice/transpose.py
@@ -9,3 +9,14 @@ one argument: A list that contains lists. and returns a modified list.
 Your task is to swap the rows and columns of the list. 
 This is usually referred to as transpose in mathematics.'''
 
+#
+
+def transpose(lst):
+    transposeLst = []
+    for i in len(lst):
+        print(lst)
+
+liist= [[1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+transpose(liist)
