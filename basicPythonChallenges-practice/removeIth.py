2fac1eb1101a1d322287034062078597e3b433a6
Oxmalik <muhammadm4lik@gmail.com>
2024-11-19 19:18:57 -0500

    remove update

--- basicPythonChallenges-practice/removeIth.py
+++ basicPythonChallenges-practice/removeIth.py
@@ -1,5 +1,16 @@
+#Muhammad Malik
+#Remove iTh char
+#5/4/2024
 
-
-
-'''Write a function named remove that gets a string and an integer and returns the string without a letter
+'''Write a function named remove that gets 
+a string and an integer and returns the string without a letter
  in the index corresponding to the integer.'''
+
+def remove(s, i):
+    spltChars = s.split()
+    print( 'spltChars', spltChars)
+    
+    tmpStr = []
+    fnlStr = []
+
+    tm
