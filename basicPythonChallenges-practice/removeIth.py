b8c0260e700a138f5560fe0ed93106f9fce4b287
Oxmalik <muhammadm4lik@gmail.com>
2024-11-28 17:41:53 -0500

    remove solution

--- basicPythonChallenges-practice/removeIth.py
+++ basicPythonChallenges-practice/removeIth.py
@@ -1,20 +1,25 @@
 #Muhammad Malik
 #Remove iTh char
-#5/4/2024
+#11/24/2024
 
 '''Write a function named remove that gets 
 a string and an integer and returns the string without a letter
  in the index corresponding to the integer.'''
 
 def remove(s, i):
-    spltChars = s.split()
-    print( 'spltChars', spltChars)
-    
-    tmpStr = []
-    # fnlStr = []
 
-    tmpStr = spltChars.replace('s','')
-    print('tmpStr: ', tmpStr)
+    newStr = s[:i] + s[i+1:]
+    print('newStr: ', newStr)
+
+
+    # spltChars = s.split()
+    # print( 'spltChars', spltChars)
+    
+    # tmpStr = []
+    # # fnlStr = []
+
+    # tmpStr = spltChars.replace('s','')
+    # print('tmpStr: ', tmpStr)
 
 chars='this Is A String'
 remove(chars, 1)
