0c625e9ec04b231de3c2843cc256735d40970d19
Oxmalik <muhammadm4lik@gmail.com>
2026-02-01 22:14:18 -0500

    area if circle

---
+++ basicPythonChallenges-practice/area-of-circle.py
@@ -0,0 +1,16 @@
+# Create a Python program that calculates the area of a circle 
+# using a constant for the value of PI. Define a constant named PI and set
+#  its value to 3.14159. Then, prompt the user to enter the radius of the circle. 
+# Calculate the area of the circle using the formula (area = PI * radius**2) and print the result.
+
+# Remember to follow the naming convention for constants by using all uppercase letters for
+#  the variable PI.
+
+PI = 3.14159
+radius = float(input())
+# area = 0.0
+
+# radius = input()
+
+area = PI * radius**2
+print(area)
