efa995daebdd8152c5763b7c1a7b49ebd01785c9
Oxmalik <muhammadm4lik@gmail.com>
2026-02-07 23:08:11 -0500

    multi variable

---
+++ basicPythonChallenges-practice/multi_variable.py
@@ -0,0 +1,17 @@
+# Write a Python program that performs the following tasks:
+
+# Assign values to three variables name, age, and city in a single line. Set name to "Alice", 
+# age to 30, and city to "New York".
+
+# Assign the value 100 to three variables x, y, and z in a single line.
+
+# Create a list named colors containing the values "red", "green", and "blue". 
+# Assign these values to three variables color1, color2, and color3 in a single line.
+
+name, age, city = "Alice", 30, "New York"
+
+x = y = z = 100
+
+colors = ["red", "green", "blue"]
+
+color1,  color2, color3 = colors
