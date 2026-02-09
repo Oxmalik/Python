aaccfba461b12105089f3daf311651b388cd7218
Oxmalik <muhammadm4lik@gmail.com>
2026-02-09 18:18:03 -0500

    variable swap

---
+++ basicPythonChallenges-practice/swapping-variables.py
@@ -0,0 +1,15 @@
+# Write a Python program to swap the values of two variables without using a temporary variable.
+# Initialize two variables, x and y, with the values 5 and 10 respectively. 
+# Swap their values and then print them.
+
+# Initialize variables x and y
+x = 5
+y = 10
+
+# Swap the values of x and y
+x , y = y , x
+
+
+# Print the swapped values
+print(f"x: {x}")
+print(f"y: {y}")
